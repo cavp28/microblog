56fe7b50e17a2ec69b12854388f7e532279ed547
César Vargas <ca.vargas28@gmail.com>
2025-02-06 22:00:43 -0400

    Add basic template structure

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
+from flask import render_template
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, world!"
+    user = {"username": "César"}
+    posts = [
+        {"author": {"username": "Paola"}, "body": "Beautiful morning in Santiago"},
+        {
+            "author": {"username": "Sandino"},
+            "body": "Moving to California soon. Summer 2025 here we go!",
+        },
+        {"author": {"username": "Welling"}, "body": "Sunny day in Berlin today! Yeah!"},
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
